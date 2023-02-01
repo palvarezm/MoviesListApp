a4b017f0a0a7d12d2a614ba5bb41fa1634b8e713
Paul <palvareze7@gmail.com>
2023-02-01 17:36:39 -0500

    Initial Commit

---
+++ MoviesListApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoviesListApp
+//
+//  Created by Paul Alvarez on 1/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoviesListApp/MoviesListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesListAppApp.swift
+//  MoviesListApp
+//
+//  Created by Paul Alvarez on 1/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
